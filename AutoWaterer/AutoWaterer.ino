5b4820d897084b36ec797487b513c0d69ce08a19
MrRenma <mjsquires47@gmail.com>
2020-03-11 13:26:47 +0000

    Watering mechanism now triggers every 12 hours and the moisture levels are checked every hour

--- AutoWaterer/AutoWaterer.ino
+++ AutoWaterer/AutoWaterer.ino
@@ -2,31 +2,33 @@
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x3F,16,2);
 int val;
+unsigned long currentTime;
+unsigned long startTime;
+unsigned long period=43200000;
 void setup() {
   lcd.init();
   lcd.backlight();
   lcd.setCursor(1,0);
   lcd.print("Hello there");
+  startTime=millis();
   Serial.begin(9600);
   pinMode(13, OUTPUT);
 
 }
 
 void loop() {
-  //Check the reading of the soil moisture sensor every five minutes. If it's below a certain
-  //level, water the plant
+  //Check the reading of the soil moisture sensor every hour. If it's below a certain level, water the plant.
+  //Also, if it hasn't been watered in 12 hours, water it anyway.
+  currentTime = millis();
   val = analogRead(A1);
-  Serial.println(val);
-  if (val<=365){
+  if (val<=430 || currentTime - startTime >= period){
+    startTime = currentTime;  
     digitalWrite(13, HIGH); 
-    delay(5000);
-    digitalWrite(13, LOW);
-  }
-  else{
+    delay(10000);
     digitalWrite(13, LOW);
   }
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print(val);
-  delay(600000);
+  delay(3600000);
 }
